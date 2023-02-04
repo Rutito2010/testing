f9b22742978deabf485851c34a4ef1fe64d868f4
Ivan Passalia <kakaroto11@live.com.ar>
2023-02-04 17:06:05 -0300

    subida

--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -1,14 +1,14 @@
-import path from "path";
-import { fileURLToPath } from "url";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const options = {
   fileSystem: {
-    pathProducts: "Products.json",
-    pathCarts: "Carritos.json",
-    pathMessages: "Messages.json",
+    pathProducts: 'Products.json',
+    pathCarts: 'Carritos.json',
+    pathMessages: 'Messages.json',
   },
   /*mariaDb: {
     client: "mysql",
@@ -16,18 +16,18 @@ export const options = {
       host: "127.0.0.1",
       user: "root",
       password: "",
-      database: "desafiosql",
+      database: "desafiosql"
     },
   },
   */ sqliteDb: {
-    client: "sqlite3",
+    client: 'sqlite3',
     connection: {
-      filename: path.join(__dirname, "../db/database.sqlite"),
+      filename: path.join(__dirname, '../db/database.sqlite'),
     },
     useNullAsDefault: true,
   },
   mongo: {
-    url: "mongodb+srv://smposse:coderMongo2022@cluster0.94d5car.mongodb.net/ecommerceDB?retryWrites=true&w=majority",
+    url: 'mongodb+srv://smposse:coderMongo2022@cluster0.94d5car.mongodb.net/ecommerceDB?retryWrites=true&w=majority',
   },
   /*firebase: {
     pathProducts: productCollection,
